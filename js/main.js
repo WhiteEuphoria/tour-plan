526c9e70151fb37a08ae52834dfe2290a53aa047
WhiteEuphoria <prognikitos@gmail.com>
2020-10-10 12:56:04 +0300

    create: added slider remote for keyboard

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-var mySwiper = new Swiper('.swiper-container', {
-    // Optional parameters
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
 
-    loop: true,
+  loop: true,
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.slider-button--next',
-        prevEl: '.slider-button--prev',
-    },
-    effect: "cube",
-
-})
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
